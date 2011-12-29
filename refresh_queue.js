c9cf74ee096cc440585fc638fd5fa1c38079630f
shijie.chen <shijie.chen@gmail.com>
2011-12-29 19:00:32 +0800

    配置文件增加对refresh_queue.js的支持

--- refresh_queue.js
+++ refresh_queue.js
@@ -6,19 +6,19 @@ var configs = JSON.parse(fs.readFileSync(config, 'utf8'));
 var logger = require('./lib/logger').logger;
 var _logger = logger(__dirname + '/log/refresh.log');
 var utils = require('./lib/utils');
-var mysql = new MySqlClient('172.16.33.237', 3306, 'stockradar', 'stockradar', 'stock_radar');
-var queue = require('../queuer/lib/queue');
+var mysql = new MySqlClient(configs.mysql.baseurl.host, configs.mysql.baseurl.port, configs.mysql.baseurl.username, configs.mysql.baseurl.password, configs.mysql.baseurl.database);
+var queue = require('queuer');
 var q4url = queue.getQueue('http://' + configs.queue_server.host + ':' + configs.queue_server.port + '/' + configs.queue_server.queue_path, configs.spider_monitor_queue);
 
 var last_run_time = 0;
 var refresh_queue = function() {
   var current_time = utils.getTimestamp();
   var hour = new Date().getHours();
-  var time_step = 5 * 60;
-  if(hour > 22 || hour < 8) {
+  var time_step = 15 * 60;
+  if (hour > 22 || hour < 8) {
     time_step = 30 * 60;
   }
-  if(current_time - last_run_time < time_step) {
+  if (current_time - last_run_time < time_step) {
     return;
   }
   last_run_time = current_time;
@@ -26,18 +26,15 @@ var refresh_queue = function() {
   mysql.get_base_url(fetch_time, function(result) {
     var length = result.length;
     for ( var i = 0; i < length; i++) {
-      var task = 'mysql://172.16.33.237:3306/stock_radar?baseurl#' + result[i].id;
+      var task = 'mysql://' + configs.queue_server.host + ':' + configs.queue_server.port + '/' + configs.mysql.baseurl.database + '?baseurl#' + result[i].id;
       _logger.info(task);
       q4url.enqueue(task);
     }
   });
 };
 
-
 refresh_queue();
 
-/*
 setInterval(function() {
-  //refresh_queue();
-}, 60*1000);
-*/
+  refresh_queue();
+}, 60 * 1000);
