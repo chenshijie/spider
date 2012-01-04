69266a6f6b7fd3f7753f64cfa929470184f5da2d
chenshijie <shijie.chen@gmail.com>
2012-01-04 18:11:45 +0800

    增加缓存功能

--- spider_server.js
+++ spider_server.js
@@ -12,8 +12,11 @@ var configs = JSON.parse(fs.readFileSync(config, 'utf8'));
 var _logger = logger(__dirname + '/' + configs.log.file);
 var queue = require('queuer');
 
-var db_options = configs.mysql;
+var redis = require("redis");
+var redisClient = redis.createClient(configs.redis.port, configs.redis.host);
+redisClient.select(configs.redis.db);
 
+var db_options = configs.mysql;
 var databases = {};
 var spiders = [];
 
@@ -21,36 +24,39 @@ var de = require('devent').createDEvent('spider');
 
 fs.writeFileSync(__dirname + '/run/server.lock', process.pid.toString(), 'ascii');
 
-  de.on('queued', function(queue) {
-    // 同时多个队列进入时会调用spiders_run()多次。
-    if (queue == 'url') {
-      //console.log('SERVER: ' + queue + "  received task");
-      // spiders_run();
-    }
-  });
+de.on('queued', function(queue) {
+  // 同时多个队列进入时会调用spiders_run()多次。
+  if (queue == 'url') {
+    // console.log('SERVER: ' + queue + " received task");
+    // spiders_run();
+  }
+});
 
 var queue4page = queue.getQueue('http://' + configs.queue_server.host + ':' + configs.queue_server.port + '/' + configs.queue_server.queue_path, configs.spider_generate_queue);
 // init spider
 for ( var i = 1; i < configs.spider_count + 1; i++) {
-  var spider = new Spider('spider_' + i);
+  var spider = new Spider('spider_' + i, configs.cache_time || 300);
   spider.on('spider_ok', function(task) {
-    //console.log('SERVER: ' + this.name + ' task-finished emmited');
+    // console.log('SERVER: ' + this.name + ' task-finished emmited');
     // var task_uri = utils.buildTaskURI(task.task);
-    //var task_uri = task.task.original_task.uri;
-    //console.log('TASK_FINISHED: ' + this.name + ' : ' + task_uri + ' will be finished!');
+    // var task_uri = task.task.original_task.uri;
+    // console.log('TASK_FINISHED: ' + this.name + ' : ' + task_uri + ' will be
+    // finished!');
     var run_time = utils.getTimestamp() - task.task.in_time;
     _logger.info([ 'TASK_FINISHED', this.name, 'RETRY:' + task.task.original_task.retry, task.task.original_task.uri, 'RUN_TIME:' + run_time ].join("\t"));
     de.emit('task-finished', task.task.original_task);
     if (task.new_task_id > 0) {
       var new_task = utils.buildTaskURI({ protocol : task.task.protocol, hostname : task.task.hostname, port : task.task.port, database : task.task.database, table : 'page_content', id : task.new_task_id });
-      //console.log('NEWTASK: ' + this.name + ' : ' + new_task + ' will add to queue');
+      // console.log('NEWTASK: ' + this.name + ' : ' + new_task + ' will add to
+      // queue');
       _logger.info([ 'NEWTASK', this.name, 'RETRY:' + task.task.original_task.retry, task.task.original_task.uri, new_task ].join("\t"));
       queue4page.enqueue(new_task);
     }
   });
 
   spider.on('spider_error', function(data) {
-    //console.log('TASK_ERROR: ' + this.name + ' : task-error emmited : ' + data.task.original_task.uri + ' ERROR_MSG: ' + data.msg);
+    // console.log('TASK_ERROR: ' + this.name + ' : task-error emmited : ' +
+    // data.task.original_task.uri + ' ERROR_MSG: ' + data.msg);
     var run_time = utils.getTimestamp() - data.task.in_time;
     _logger.info([ 'TASK_ERROR', this.name, data.task.original_task.retry, data.task.original_task.uri, 'RUN_TIME:' + run_time ].join("\t"));
     de.emit('task-error', data.task.original_task);
@@ -94,7 +100,7 @@ var spider2run = function(spider) {
         databases[key] = mysql;
       }
       var db = databases[key];
-      spider.run(task_obj, db, _logger);
+      spider.run(task_obj, db, redisClient, configs.redis.db, _logger);
     }
   });
 };
